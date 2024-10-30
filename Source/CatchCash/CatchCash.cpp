6e9d4e1d79015d59248fd1ecbc593066ba4c05fd
Limjiyoung <22314671@naver.com>
2024-10-30 18:38:58 +0900

    steam
    steam연결

---
+++ Source/CatchCash/CatchCash.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatchCash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatchCash, "CatchCash" );
+ 
---
+++ Source/CatchCash/CatchCashGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CatchCashGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACatchCashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACatchCashGameMode();
+};
+
+
+
